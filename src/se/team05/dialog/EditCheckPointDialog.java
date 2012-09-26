9b5947dbfd40d123be516365cc91c363110f6c07
Snadde <thituson@gmail.com>
2012-09-26 17:56:31 +0200

    Add: classes for the checkpoint feature. CheckPoint is a overlayitem with some extra attributes. CheckPointOverlay has a list with checkpoints. EditCheckPointDialog is the dialog that pops up when a checkpoint is created or tapped, it can change values and delete the checkpoint. edit_checkpoint:dialog.xml is the layout for the dialog

---
+++ src/se/team05/dialog/EditCheckPointDialog.java
@@ -0,0 +1,134 @@
+/**
+	This file is part of Personal Trainer.
+
+    Personal Trainer is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    any later version.
+
+    Personal Trainer is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Personal Trainer.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package se.team05.dialog;
+
+//import com.example.testmap.CheckPoint;
+//import com.example.testmap.CheckPointOverlay;
+//import com.example.testmap.R;
+//import com.example.testmap.R.id;
+//import com.example.testmap.R.layout;
+
+import se.team05.R;
+import se.team05.overlay.CheckPoint;
+import se.team05.overlay.CheckPointOverlay;
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.TextView;
+
+/**
+ * This is the dialog that pops up when a checkpoint is created or touched. The
+ * Dialog has settings such as name & radius and buttons for deleting and saving
+ * the checkpoint.
+ * 
+ * @author Patrik Thituson & Daniel Kvist
+ * @version 1.0
+ * 
+ */
+public class EditCheckPointDialog extends Dialog implements View.OnClickListener, OnSeekBarChangeListener
+{
+	public interface Callbacks
+	{
+		public void onDelete();
+
+	}
+
+	private CheckPointOverlay callBack;
+	private CheckPoint checkPoint;
+	private TextView nameTextField;
+	private TextView radiusTextField;
+
+	public EditCheckPointDialog(Context context, CheckPointOverlay callback, CheckPoint checkPoint)
+	{
+		super(context);
+		this.callBack = callback;
+		this.checkPoint = checkPoint;
+		setCancelable(false); //Possible change to setCanceledOnTouchOutside(false)
+	}
+
+	/**
+	 * Initate the dialog with layout from xml file and sets the default values
+	 * on the attributes
+	 */
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.edit_checkpoint_dialog);
+		setTitle("Edit CheckPoint");
+		findViewById(R.id.delete_button).setOnClickListener(this);
+		findViewById(R.id.save_button).setOnClickListener(this);
+		nameTextField = (TextView) findViewById(R.id.name);
+		nameTextField.setText(checkPoint.getName());
+		SeekBar seekBar = (SeekBar) findViewById(R.id.seekBar1);
+		seekBar.setOnSeekBarChangeListener(this);
+		seekBar.setProgress(checkPoint.getRadius());
+	}
+
+	/**
+	 * Deletes or saves the product. Deletes use a callback to
+	 * CheckpointOverlay. Save uses the setters to set the attributes
+	 */
+	@Override
+	public void onClick(View v)
+	{
+		switch (v.getId())
+		{
+			case R.id.delete_button:
+				callBack.onDelete();
+				dismiss();
+				break;
+			case R.id.save_button:
+				checkPoint.setName(nameTextField.getText().toString());
+				radiusTextField = (TextView) findViewById(R.id.radius_text);
+				String radiusString = radiusTextField.getText().toString();
+				int radius = Integer.parseInt(radiusString);
+				checkPoint.setRadius(radius);
+				dismiss();
+				break;
+			default:
+				break;
+		}
+	}
+
+	/**
+	 * Sets the radius in the textfield when the seekbar is changed
+	 */
+	@Override
+	public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser)
+	{
+		radiusTextField.setText("" + progress);
+	}
+
+	@Override
+	public void onStartTrackingTouch(SeekBar seekBar)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onStopTrackingTouch(SeekBar seekBar)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
