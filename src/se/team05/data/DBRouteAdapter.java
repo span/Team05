45eef75696e8231a63eafd12ee501bd59715d287
Daniel Kvist <daniel@danielkvist.net>
2012-10-01 18:40:50 +0200

    Change: Refactored helper and adapter for better cohesion

--- src/se/team05/data/DBRouteAdapter.java
+++ src/se/team05/data/DBRouteAdapter.java
@@ -1,21 +1,33 @@
 package se.team05.data;
 
+
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 
 public class DBRouteAdapter
 {
+	public static final String TABLE_ROUTES = "routes";
+	public static final String COLUMN_ID = "id";
+	public static final String COLUMN_NAME = "name";
+	public static final String COLUMN_DESCRIPTION = "description";
+	public static final String COLUMN_TYPE = "type";
+	public static final String COLUMN_TIMECOACH = "timecoach";
+	public static final String COLUMN_LENGTHCOACH = "lengthcoach";
+
+	public static final String DATABASE_CREATE_ROUTE_TABLE = "create table " + TABLE_ROUTES + "(" + COLUMN_ID
+			+ " integer primary key autoincrement, " + COLUMN_NAME + " text not null, " + COLUMN_DESCRIPTION
+			+ " text not null," + COLUMN_TYPE + " integer not null," + COLUMN_TIMECOACH + " integer not null,"
+			+ COLUMN_LENGTHCOACH + " integer not null);";
 
 	private DatabaseHelper databaseHelper;
 	private SQLiteDatabase db;
-	
+
 	public DBRouteAdapter(Context context)
 	{
-		// TODO Auto-generated constructor stub
 		databaseHelper = new DatabaseHelper(context);
 	}
-	
+
 	public void open()
 	{
 		db = databaseHelper.getWritableDatabase();
@@ -25,15 +37,15 @@ public class DBRouteAdapter
 	{
 		databaseHelper.close();
 	}
-	
+
 	public void createRoute()
 	{
 		ContentValues values = new ContentValues();
-		values.put(DatabaseHelper.COLUMN_NAME, "myRoute");
-		values.put(DatabaseHelper.COLUMN_DESCRIPTION, "myDescription");
-		values.put(DatabaseHelper.COLUMN_TYPE, 0);
-		values.put(DatabaseHelper.COLUMN_TIMECOACH, 0);
-		values.put(DatabaseHelper.COLUMN_LENGTHCOACH, 0);
-		db.insert(DatabaseHelper.TABLE_ROUTES, null, values);
+		values.put(COLUMN_NAME, "myRoute");
+		values.put(COLUMN_DESCRIPTION, "myDescription");
+		values.put(COLUMN_TYPE, 0);
+		values.put(COLUMN_TIMECOACH, 0);
+		values.put(COLUMN_LENGTHCOACH, 0);
+		db.insert(TABLE_ROUTES, null, values);
 	}
 }
