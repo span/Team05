22d38c568dd7e5972d1f3a8385d7d0dcc6ed059b
Daniel Kvist <daniel@danielkvist.net>
2012-10-15 13:06:01 +0200

    Add: documentation and missing licenses

--- src/se/team05/dialog/AlertDialogFactory.java
+++ src/se/team05/dialog/AlertDialogFactory.java
@@ -1,3 +1,21 @@
+/**
+	This file is part of Personal Trainer.
+	
+	Personal Trainer is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	any later version.
+	
+	Personal Trainer is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+	
+	You should have received a copy of the GNU General Public License
+	along with Personal Trainer.  If not, see <http://www.gnu.org/licenses/>.
+
+    (C) Copyright 2012: Daniel Kvist, Henrik Hugo, Gustaf Werlinder, Patrik Thitusson, Markus Schutzer
+ */
 package se.team05.dialog;
 
 import se.team05.R;
@@ -10,15 +28,37 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.widget.Toast;
 
+/**
+ * This factory class is used to create alert dialog instances for the save
+ * result dialog, confirm back press dialog and others. It always returns a
+ * created instance so remember to call show() on the alert dialog that is
+ * returned.
+ * 
+ * @author Daniel Kvist, Patrik Thitusson
+ * 
+ */
 public class AlertDialogFactory
 {
+	/**
+	 * Creates a new "save result?" dialog. It needs a context, route and result
+	 * to be displayed. If the user saves the result a toast is shown as
+	 * confirmation. If the result is not saved the dialog is simply cancelled.
+	 * 
+	 * @param context
+	 *            the context to operate in
+	 * @param route
+	 *            the route which was executed
+	 * @param result
+	 *            the result of the route
+	 * @return a new alert dialog
+	 */
 	public static AlertDialog newSaveResultDialog(final Context context, final Route route, final Result result)
 	{
 		String giveUserDistanceString = context.getString(R.string.distance_of_run) + String.valueOf((int) route.getTotalDistance())
 				+ context.getString(R.string.km) + "\n";
 		String giveUserTimeString = context.getString(R.string.time_) + route.getTimePassedAsString() + "\n\n";
 		String giveUserResultData = giveUserDistanceString + giveUserTimeString;
-		
+
 		return new AlertDialog.Builder(context).setTitle(R.string.save_result_)
 				.setMessage(giveUserResultData + context.getString(R.string.do_you_want_to_save_this_result_))
 				.setPositiveButton(R.string.yes, new DialogInterface.OnClickListener()
@@ -38,6 +78,16 @@ public class AlertDialogFactory
 				}).create();
 	}
 
+	/**
+	 * Creates a new "confirm back" dialog that can be shown to the user when
+	 * he/she presses the back button. A good use is for example when a route is
+	 * being executed and the back button is mistakenly pressed. If the user
+	 * presses the positive button the activity (context) is finished.
+	 * 
+	 * @param context
+	 *            the context to operate in and finish if positive answer
+	 * @return a new alert dialog
+	 */
 	public static AlertDialog newConfirmBackDialog(final Context context)
 	{
 		return new AlertDialog.Builder(context).setTitle(R.string.discard_route_)
--- src/se/team05/util/Utils.java
+++ src/se/team05/util/Utils.java
@@ -1,3 +1,21 @@
+/**
+	This file is part of Personal Trainer.
+	
+	Personal Trainer is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	any later version.
+	
+	Personal Trainer is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+	
+	You should have received a copy of the GNU General Public License
+	along with Personal Trainer.  If not, see <http://www.gnu.org/licenses/>.
+
+    (C) Copyright 2012: Daniel Kvist, Henrik Hugo, Gustaf Werlinder, Patrik Thitusson, Markus Schutzer
+ */
 package se.team05.util;
 
 import se.team05.R;
@@ -7,17 +25,26 @@ import android.os.PowerManager;
 import android.os.PowerManager.WakeLock;
 import android.util.Log;
 
+/**
+ * This utility class is used to perform system services such as acquiring and
+ * releasing the wake lock and also to start a timer that updates a callback
+ * through the Callbacks interface.
+ * 
+ * @author Daniel Kvist
+ * 
+ */
 public class Utils
 {
 	public interface Callbacks
 	{
 		public void onTimerTick();
 	}
+
 	private static final String TAG = "Personal trainer";
 	private static Runnable runnable;
 	private static Handler handler;
 	private static WakeLock wakeLock;
-	
+
 	/**
 	 * Acquires the wake lock from the system if it is available and not already
 	 * held.
@@ -76,6 +103,9 @@ public class Utils
 		handler.postDelayed(runnable, 0);
 	}
 
+	/**
+	 * Stops the timer
+	 */
 	public static void stopTimer()
 	{
 		handler.removeCallbacks(runnable);
