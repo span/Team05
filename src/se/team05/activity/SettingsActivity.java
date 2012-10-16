a0e2274ba8c7d2c8036b87b1ed696f266584a01f
Elefantbilen <markusschytzer@hotmail.com>
2012-10-16 23:44:17 +0200

    Change: Refactored activity.

--- src/se/team05/activity/SettingsActivity.java
+++ src/se/team05/activity/SettingsActivity.java
@@ -1,9 +1,6 @@
 package se.team05.activity;
 
 import se.team05.R;
-import se.team05.R.layout;
-import se.team05.R.menu;
-import se.team05.content.CalorieCounter;
 import se.team05.content.Settings;
 import android.os.Bundle;
 import android.app.Activity;
@@ -23,19 +20,14 @@ public class SettingsActivity extends Activity implements View.OnClickListener
 {
 	EditText nameEdit;
 	EditText weightEdit;
-	private String userName;
-	private String userWeightString;
-	private int userWeight;
-	private boolean isLengthSi;
-	private boolean isWeightSi;
-	private boolean preferredActivityRunning;
+
 	private Button saveSettings;
 	private RadioButton radioWalking;
 	private RadioButton radioRunning;
-	private RadioButton radioLengthSI;
-	private RadioButton radioLengthEng;
-	private RadioButton radioWeightSI;
-	private RadioButton radioWeightEng;
+//	private RadioButton radioLengthSI;
+//	private RadioButton radioLengthEng;
+//	private RadioButton radioWeightSI;
+//	private RadioButton radioWeightEng;
 	
 	static SharedPreferences sharedPreferences;
 	private Settings settings;
@@ -47,83 +39,59 @@ public class SettingsActivity extends Activity implements View.OnClickListener
         setContentView(R.layout.activity_settings);
         getActionBar().setDisplayHomeAsUpEnabled(true);
         settings = new Settings(this);
-        
-        userName = settings.getUserName();
-        userWeight = settings.getUserWeight();
-        isLengthSi = settings.isSILength();
-        isWeightSi = settings.isSIWeight();
-        System.out.println("ONCREATE VIKT: " + settings.isSIWeight());
-        preferredActivityRunning = settings.isPrefActivityRunning();
 
-        userWeightString = "" + userWeight;
-
-        setUpButtons();
-        
+        setUpButtons();     
     }
     
     public void onResume(Bundle savedInstanceState)
     {
 		super.onResume();
-		
-        userName = settings.getUserName();
-        userWeight = settings.getUserWeight();
-        isLengthSi = settings.isSILength();
-        System.out.println("ONRESUME VIKT: " + settings.isSIWeight());
-        isWeightSi = settings.isSIWeight();
-        userWeightString = "" + userWeight;
     }
     
     private void setUpButtons()
     {
     	radioWalking = (RadioButton) findViewById(R.id.radio_walking);
         radioRunning = (RadioButton) findViewById(R.id.radio_running);
-     	radioWalking.setChecked(!preferredActivityRunning);
-     	radioRunning.setChecked(preferredActivityRunning);
-     	
-    	radioLengthSI = (RadioButton) findViewById(R.id.radio_length_si);
-    	radioLengthEng = (RadioButton) findViewById(R.id.radio_length_eng);
-    	radioLengthSI.setChecked(isLengthSi);
-    	radioLengthEng.setChecked(!isLengthSi);
-    	
-    	radioWeightSI = (RadioButton) findViewById(R.id.radio_weight_si);
-    	radioWeightEng = (RadioButton) findViewById(R.id.radio_weight_eng);
-    	radioWeightSI.setChecked(isWeightSi);
-    	radioWeightEng.setChecked(!isWeightSi);
-    	
+     	radioWalking.setChecked(!settings.isPrefActivityRunning());
+     	radioRunning.setChecked(settings.isPrefActivityRunning());
+
     	nameEdit = (EditText) findViewById(R.id.edit_name);
-        nameEdit.setText(userName);
+        nameEdit.setText(settings.getUserName());
         weightEdit = (EditText) findViewById(R.id.edit_weight);
-        weightEdit.setText(userWeightString);
+        weightEdit.setText(String.valueOf(settings.getUserWeight()));
         
         saveSettings = (Button) findViewById(R.id.save_settings);
         saveSettings.setOnClickListener(this);
+        
+//    	radioLengthSI = (RadioButton) findViewById(R.id.radio_length_si);
+//    	radioLengthEng = (RadioButton) findViewById(R.id.radio_length_eng);
+//    	radioLengthSI.setChecked(isLengthSi);
+//    	radioLengthEng.setChecked(!isLengthSi);
+//    	
+//    	radioWeightSI = (RadioButton) findViewById(R.id.radio_weight_si);
+//    	radioWeightEng = (RadioButton) findViewById(R.id.radio_weight_eng);
+//    	radioWeightSI.setChecked(isWeightSi);
+//    	radioWeightEng.setChecked(!isWeightSi);
+    	
     }
 
     
 	@Override
 	public void onClick(View arg0) 
 	{
-		
-		userWeightString = weightEdit.getText().toString();
-		
-		if(testValues())
+		if(testWeight() >= 0)
 		{
+		    settings.setUserName(nameEdit.getText().toString());
+			settings.setUserWeight(testWeight());
+			settings.setPrefActivityRunning(radioRunning.isChecked());
 
-		    userName = nameEdit.getText().toString();
-			settings.setUserName(userName);
-			settings.setUserWeight(userWeight);
-			settings.setPrefActivityRunning(preferredActivityRunning);
-			settings.setSILength(isLengthSi);
-			settings.setSIWeight(isWeightSi);
 			settings.commitChanges();
 
 			CharSequence text = getString(R.string.settings_saved);
 			int duration = Toast.LENGTH_SHORT;
 
 			Toast toast = Toast.makeText(this, text, duration);
-			toast.show();
-
-		    
+			toast.show();    
 		}
 		else
 		{
@@ -133,69 +101,29 @@ public class SettingsActivity extends Activity implements View.OnClickListener
 					{
 						public void onClick(DialogInterface dialog, int id)
 						{
-							//TODO PUT SOMETHING HERE?
 						}
 					}).create();
 			alertDialog.show();
 		}
-		System.out.println("FÄRDIG I KNAPPEN");
 	}
-	
 	
 	public void onRadioButtonClicked(View view) 
 	{
-	    boolean checked = ((RadioButton) view).isChecked();
-	
-	    switch(view.getId()) 
-	    {
-	        case R.id.radio_running:
-	            if (checked)
-	                preferredActivityRunning = true;
-	            System.out.println("RUNNING JA");
-	            break;
-	        case R.id.radio_walking:
-	            if (checked)
-	            	preferredActivityRunning = false;
-	            System.out.println("RUNNING NEJ");
-	            break;
-	            
-	        case R.id.radio_length_si:
-	        	if(checked)
-	        		isLengthSi = true;
-	        	 System.out.println("KM");
-	        	break;
-	        case R.id.radio_length_eng:
-	        	if(checked)
-	        		isLengthSi = false;
-	        	 System.out.println("MILES");
-	        	break;
-	        	
-	        case R.id.radio_weight_si:
-	        	if(checked)
-	        		isWeightSi = true;
-	        	 System.out.println("kg");
-	        	break;
-	        case R.id.radio_weight_eng:
-	        	if(checked)
-	        		isWeightSi = false;
-	        	 System.out.println("POUNDS");
-	        	break;
-	    }
+
 	}
     
-    private boolean testValues()
+    private int testWeight()
     {
+    	int userWeight;
     	try
     	{
-    		userWeight = Integer.parseInt(userWeightString);
+    		userWeight = Integer.parseInt(weightEdit.getText().toString());
     	}
     	catch(NumberFormatException e)
     	{
-
-			return false;
+			userWeight = -1;
     	}
-
-    	return true;
+    	return userWeight;
     }
     
     
@@ -215,5 +143,4 @@ public class SettingsActivity extends Activity implements View.OnClickListener
         }
         return super.onOptionsItemSelected(item);
     }
-
 }
